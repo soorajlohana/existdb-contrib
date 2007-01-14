497d167979b8ec53a549e779f197479d7bcc78f7
dizzzz <dizzzz@66bb6a36-0227-0410-b297-1b65bf2f2943>
2007-01-14 16:18:38 +0000

    Add project files

---
+++ trunk/existdb-contrib/src/existdbcontrib/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on January 14, 2007, 5:12 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package existdbcontrib;
+
+/**
+ *
+ * @author wessels
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

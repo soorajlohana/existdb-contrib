43dd35326c8c1fddee20b02901a5b91fdfb6f855
dizzzz <dizzzz@66bb6a36-0227-0410-b297-1b65bf2f2943>
2007-01-17 20:47:20 +0000

    Added xmldb URLStreamHandlerFactory Added tests Added LGPL header

--- trunk/existdb-contrib/test/org/exist/protocols/eXistURLStreamHandlerFactoryTest.java
+++ trunk/existdb-contrib/test/org/exist/protocols/eXistURLStreamHandlerFactoryTest.java
@@ -20,9 +20,8 @@
  *  $Id$
  */
 
-package org.exist.protocols.xmldb;
+package org.exist.protocols;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -37,37 +36,37 @@ import org.apache.log4j.BasicConfigurator;
  *
  * @author wessels
  */
-public class ConnectionTest extends TestCase {
+public class eXistURLStreamHandlerFactoryTest extends TestCase {
     
     private static boolean firstTime=true;
     
-    public ConnectionTest(String testName) {
+    public eXistURLStreamHandlerFactoryTest(String testName) {
         super(testName);
-    }
-    
-    protected void setUp() throws Exception {
-        if(firstTime){
-            BasicConfigurator.configure();
-            System.setProperty( "java.protocol.handler.pkgs", "org.exist.protocols" );
-            firstTime=false;
-        }
     }
     
     protected void tearDown() throws Exception {
     }
     
+    protected void setUp() throws Exception {
+        if(firstTime){
+            BasicConfigurator.configure();
+            firstTime=false;
+        }
+    }
+    
     /**
-     * Test of connect method, of class org.exist.protocols.xmldb.Connection.
+     * Test of createURLStreamHandler method, of class org.exist.protocols.eXistURLStreamHandlerFactory.
      */
-    public void testGet1() {
-        System.out.println("testGet1");
+    public void testXMLDBURLStreamHandler() {
+        System.out.println("createURLStreamHandler");
+        
+        URL.setURLStreamHandlerFactory(new eXistURLStreamHandlerFactory());
+        
         try {
             // TODO add your test code.
             URL url = new URL("xmldb:exist://guest:guest@localhost:8080/exist/xmlrpc/db/shakespeare/plays/macbeth.xml");
             InputStream is = url.openStream();
-            
             copyDocument(is, System.out);
-            
             is.close();
         } catch (MalformedURLException ex) {
             fail(ex.toString());
@@ -76,52 +75,7 @@ public class ConnectionTest extends TestCase {
             fail(ex.toString());
             ex.printStackTrace();
         }
-    }
-    
-    public void testGet2() {
-        System.out.println("testGet2");
-        try {
-            // TODO add your test code.
-            URL url = new URL("xmldb:exist://guest:guest@localhost:8080/exist/xmlrpc/db/foobar/macbeth.xml");
-            InputStream is = url.openStream();
-            
-            copyDocument(is, System.out);
-
-            is.close();
-            fail("Document should not exist");
-        } catch (MalformedURLException ex) {
-            fail(ex.toString());
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            //fail(ex.toString());
-            // Expected TODO more acurate check
-            ex.printStackTrace();
-        }
-    }
-    
-    /**
-     * Test of getInputStream method, of class org.exist.protocols.xmldb.Connection.
-     */
-    public void testPut() throws Exception {
-        System.out.println("testPut");
-        // TODO add your test code.
-        try {
-            // TODO add your test code.
-            URL url = new URL("xmldb:exist://guest:guest@localhost:8080/exist/xmlrpc/db/build.xml");
-            OutputStream os = url.openConnection().getOutputStream();
-            
-            FileInputStream is = new FileInputStream("build.xml");
-            copyDocument(is, os);
-            
-            is.close();
-            os.close();
-        } catch (MalformedURLException ex) {
-            fail(ex.toString());
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            fail(ex.toString());
-            ex.printStackTrace();
-        }
+        
     }
     
     private void copyDocument(InputStream is, OutputStream os) throws IOException{
@@ -138,6 +92,6 @@ public class ConnectionTest extends TestCase {
         
     }
     
-    
+
     
 }

f59df3383d6a90ad19f5e4215a96e1c7cd500fc5
dizzzz <dizzzz@66bb6a36-0227-0410-b297-1b65bf2f2943>
2007-01-19 15:20:02 +0000

    Fiexed jUnit tests

---
+++ existdb-contrib/src/org/exist/protocols/Credentials.java
@@ -0,0 +1,21 @@
+/*
+ * Credentials.java
+ *
+ * Created on January 19, 2007, 4:04 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.exist.protocols;
+
+/**
+ *
+ * @author dwessels
+ */
+public class Credentials {
+    
+    public String username;
+    public String password;
+    
+}

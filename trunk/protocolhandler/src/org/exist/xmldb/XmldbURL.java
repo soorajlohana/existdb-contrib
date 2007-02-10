8747c7ede71c51ff75d99ca876130cec642be616
dizzzz <dizzzz@66bb6a36-0227-0410-b297-1b65bf2f2943>
2007-02-10 21:09:54 +0000

    Refactoring and javadoc updates

--- trunk/protocolhandler/src/org/exist/xmldb/XmldbURL.java
+++ trunk/protocolhandler/src/org/exist/xmldb/XmldbURL.java
@@ -27,58 +27,117 @@ import java.net.URI;
 import java.net.URL;
 
 /**
- *  Equivalent to the XmldbURI class
+ *  A utility class for xmldb URLs. Since, java.net.URL is final this class
+ * acts as a wrapper, convenience methods have been added.<BR>
+ * <BR>
+ * Example:<BR>
+ * <I>xmldb:exist://username:password@hostname:8080/exist/xmlrpc/db/collection/document.xml</I><BR>
+ * <BR>
+ * Note: A collection URL ends with a "/":<BR>
+ * <I>xmldb:exist://hostname:8080/exist/xmlrpc/db/collection/</I>
+ *
+ * @see java.net.URI
+ * @see java.net.URL
+ * @see org.exist.xmldb.XmldbURI
  *
  * @author Dannes Wessels
  */
 public class XmldbURL {
     
-    private static int USERNAME=1;
-    private static int PASSWORD=2;
+    private static final int USERNAME=1;
+    private static final int PASSWORD=2;
     
     private URL myUrl;
     
-    /** Creates a new instance of XmldbURL */
+    /**
+     * Creates a new instance of XmldbURL using an XmldbURI object.
+     *
+     * @param xmldbURI Resource location.
+     * @throws java.net.MalformedURLException
+     */
     public XmldbURL(XmldbURI xmldbURI) throws MalformedURLException {
         this(xmldbURI.toURL());
     }
     
-    /** Creates a new instance of XmldbURL */
+    /**
+     * Creates a new instance of XmldbURL using an URL object.
+     * @param url Resource location.
+     * @throws java.net.MalformedURLException
+     */
     public XmldbURL(URL url) throws MalformedURLException  {
         // check protocol
-        myUrl = url;
+        if(url.getProtocol().equals("xmldb")){
+            myUrl = url;
+        } else {
+            throw new MalformedURLException("URL is not an \"xmldb:\" URL: "+url.toString() );
+        }
     }
     
-    /** Creates a new instance of XmldbURL */
+    /**
+     * Creates a new instance of XmldbURL using an URI object.
+     *
+     * @param uri Resource location.
+     * @throws java.net.MalformedURLException
+     */
     public XmldbURL(URI uri) throws MalformedURLException  {
-        myUrl=uri.toURL();
+        this(uri.toURL());
     }
     
-    /** Creates a new instance of XmldbURL */
+    /**
+     * Creates a new instance of XmldbURL using an String.
+     * @param txt Resource location.
+     * @throws java.net.MalformedURLException
+     */
     public XmldbURL(String txt) throws MalformedURLException {
-        myUrl = new URL( txt );
+        this(new URL(txt));
     }
     
+    /**
+     * xmldb:exist://<B>username:password</B>@hostname:8080/exist/xmlrpc/db/collection/document.xml
+     * @see java.net.URL#getUserInfo
+     *
+     * @return username:password
+     */
     public String getUserInfo() {
-       return myUrl.getUserInfo();
+        return myUrl.getUserInfo();
     }
     
+    /**
+     * xmldb:exist://<B>username</B>:password@hostname:8080/exist/xmlrpc/db/collection/document.xml
+     * @return username
+     */
     public String getUsername(){
         return extractCredentials(USERNAME);
     }
     
+    /**
+     * xmldb:exist://username:<B>password</B>@hostname:8080/exist/xmlrpc/db/collection/document.xml
+     * @return password
+     */
     public String getPassword(){
         return extractCredentials(PASSWORD);
     }
     
+    /**
+     * @return URL representation of location.
+     */
     public URL getURL(){
         return myUrl;
     }
     
+    /**
+     * xmldb:exist://<B>username:password@hostname:8080/exist/xmlrpc/db/collection/document.xml</B>?query#fragment
+     * @see java.net.URL#getAuthority
+     * @return authority
+     */
     public String getAuthority() {
         return myUrl.getAuthority();
     }
     
+    /**
+     * Return context
+     * @return TODO
+     */
     public String getContext() {
         return "/exist/xmlrpc"; // TODO repair
     }
@@ -87,7 +146,14 @@ public class XmldbURL {
     // /exist/xmlrpc/db/shakespeare/plays/
     // /db/shakespeare/plays/macbeth.xml
     // /db/shakespeare/plays/
+    
+    
+    /**
+     * xmldb:exist://username:password@hostname:8080/exist/xmlrpc<B>/db/collection</B>/document.xml
+     * @return collection
+     */
     public String getCollection(){
+        // TODO change hardcoded numbers
         String serverPath=myUrl.getFile();
         String collectionName=null;
         
@@ -110,6 +176,10 @@ public class XmldbURL {
         return collectionName;
     }
     
+    /**
+     * xmldb:exist://username:password@hostname:8080/exist/xmlrpc/db/collection/<B>document.xml</B>
+     * @return collection
+     */
     public String getDocumentName(){
         String serverPath=myUrl.getFile();
         String documentName=null;
@@ -126,6 +196,7 @@ public class XmldbURL {
         return documentName;
     }
     
+    // Get username or password
     private String extractCredentials(int part) {
         
         String userInfo = myUrl.getUserInfo();
@@ -164,10 +235,20 @@ public class XmldbURL {
         return null;
     }
     
+    /**
+     * <B>xmldb</B>:exist://username:password@hostname:8080/exist/xmlrpc/db/collection/document.xml
+     * @see java.net.URL#getProtocol
+     * @return protocol
+     */
     public String getProtocol(){
         return myUrl.getProtocol();
     }
     
+    /**
+     * xmldb:exist://username:password@<B>hostname</B>:8080/exist/xmlrpc/db/collection/document.xml
+     * @see java.net.URL#getProtocol
+     * @return protocol
+     */
     public String getHost(){
         String hostname=myUrl.getHost();
         if(hostname.equals("")){
@@ -175,40 +256,84 @@ public class XmldbURL {
         } else {
             return hostname;
         }
-    }    
+    }
     
+    /**
+     * xmldb:exist://username:password@hostname:<B>8080</B>/exist/xmlrpc/db/collection/document.xml
+     * @see java.net.URL#getPort
+     * @return port
+     */
     public int getPort(){
         return myUrl.getPort();
-    }    
-
+    }
+    
+    /**
+     * xmldb:exist://username:password@hostname:8080:<B>/exist/xmlrpc/db/collection/document.xml</B>
+     * @see java.net.URL#getPath
+     * @return port
+     */
     public String getPath(){
         return myUrl.getPath();
-    }    
-
+    }
+    
+    /**
+     * xmldb:exist://username:password@hostname:8080/exist/xmlrpc/db/collection/document.xml?<B>query</B>#fragment
+     * @see java.net.URL#getQuery
+     * @return query
+     */
     public String getQuery(){
         return myUrl.getQuery();
-    }    
+    }
     
+    /**
+     * xmldb:exist://username:password@hostname:8080:/exist/xmlrpc<B>/db/collection/document.xml</B>
+     * @return collectionpath
+     */
     public String getCollectionPath(){
         return myUrl.getPath().substring(13);
     }
     
+    /**
+     * Get http:// URL from xmldb:exist:// URL
+     * xmldb:exist://username:password@hostname:8080:/exist/xmlrpc/db/collection/document.xml
+     * @return http://username:password@hostname:8080:/exist/xmlrpc/db/collection/document.xml
+     */
     public String getXmlRpcURL(){
         return "http://" + myUrl.getAuthority() + getContext();
     }
     
+    /**
+     * Does the URL have at least a username?
+     * @return TRUE when URL contains username
+     */
     public boolean hasUserInfo(){
         return (getUserInfo()!=null && getUsername()!=null);
     }
-
+    
+    /**
+     * Get eXist instance name.
+     *
+     * @return eXist-db instance name, at this moment fixed to exist
+     */
     public String getInstanceName() {
         return "exist";  // No other choice
     }
     
+    /**
+     * Get textual representation of URL.
+     *
+     * @see java.net.URL#toString
+     * @return Text representation of URL.
+     */
     public String toString(){
         return myUrl.toString();
     }
     
+    /**
+     * Get information wether URL is an embedded URL.
+     *
+     * @return TRUE when URL refers to resource in embedded eXist-db.
+     */
     public boolean isEmbedded(){
         return (getHost()==null);
     }

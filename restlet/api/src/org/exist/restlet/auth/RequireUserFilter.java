0db81575af0f283763e1b55e66ea1f0476557c27
alexmilowski <alexmilowski@66bb6a36-0227-0410-b297-1b65bf2f2943>
2010-11-14 17:04:31 +0000

    added challenge support

--- restlet/api/src/org/exist/restlet/auth/RequireUserFilter.java
+++ restlet/api/src/org/exist/restlet/auth/RequireUserFilter.java
@@ -5,13 +5,18 @@
 
 package org.exist.restlet.auth;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.exist.restlet.XMLDBResource;
-import org.exist.security.User;
+import org.exist.security.Subject;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
+import org.restlet.data.ChallengeRequest;
+import org.restlet.data.ChallengeResponse;
+import org.restlet.data.ChallengeScheme;
 import org.restlet.data.Status;
 import org.restlet.routing.Filter;
 
@@ -22,6 +27,7 @@ import org.restlet.routing.Filter;
 public class RequireUserFilter extends Filter {
 
    String redirectPath;
+   String challenge;
    Set<String> groups;
 
    public RequireUserFilter() {
@@ -35,6 +41,7 @@ public class RequireUserFilter extends Filter {
       throws Exception
    {
       redirectPath = getContext().getParameters().getFirstValue("redirect");
+      challenge = getContext().getParameters().getFirstValue("challenge");
       groups = new HashSet<String>();
       String [] groupsList = getContext().getParameters().getValuesArray("groups");
       for (int i=0; groupsList!=null && i<groupsList.length; i++) {
@@ -48,11 +55,43 @@ public class RequireUserFilter extends Filter {
 
    protected int beforeHandle(Request request, Response response)
    {
-      User user = (User)request.getAttributes().get(XMLDBResource.USER_NAME);
+      Subject user = (Subject)request.getAttributes().get(XMLDBResource.USER_NAME);
       if (user==null) {
          if (redirectPath!=null) {
             response.redirectSeeOther(redirectPath);
             response.setStatus(Status.REDIRECTION_SEE_OTHER);
+         } else if (challenge!=null) {
+            ChallengeResponse authResponse = request.getChallengeResponse();
+            if (authResponse!=null) {
+               String username = authResponse.getIdentifier();
+               String password = new String(authResponse.getSecret());
+               UserManager userManager = (UserManager)request.getAttributes().get(XMLDBResource.USER_MANAGER_NAME);
+               if (userManager==null) {
+                  userManager = (UserManager)getContext().getAttributes().get(XMLDBResource.USER_MANAGER_NAME);
+               }
+
+               if (userManager!=null) {
+                  user = userManager.authenticate(username, password);
+                  if (user!=null) {
+                     SessionManager sessionManager = (SessionManager)request.getAttributes().get(XMLDBResource.SESSION_MANAGER_NAME);
+                     if (sessionManager==null) {
+                        sessionManager = (SessionManager)getContext().getAttributes().get(XMLDBResource.SESSION_MANAGER_NAME);
+                     }
+                     if (sessionManager!=null) {
+                        String sessionId = sessionManager.newSession(user);
+                        request.getAttributes().put(XMLDBResource.SESSION_NAME,sessionId);
+                     }
+                     request.getAttributes().put(XMLDBResource.USER_NAME,user);
+                     request.getAttributes().put(XMLDBResource.NEW_USER_NAME,Boolean.TRUE);
+                     return Filter.CONTINUE;
+                  }
+               }
+
+            }
+            List<ChallengeRequest> challenges = new ArrayList<ChallengeRequest>();
+            challenges.add(new ChallengeRequest(ChallengeScheme.HTTP_BASIC,challenge));
+            response.setChallengeRequests(challenges);
+            response.setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
          } else {
             response.setStatus(Status.CLIENT_ERROR_FORBIDDEN);
          }
